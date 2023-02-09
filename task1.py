38cb732907c5c4947fda1248ae23c3b0b5b9598a
mike <ytianhui2004@gmail.com>
2023-02-09 15:42:19 +0000

    clean config

--- task1.py
+++ task1.py
@@ -6,37 +6,42 @@ from utils import get_dataset
 from matplotlib.patches import Rectangle
 
 # %matplotlib inline
-dataset = get_dataset("./data/train/*.tfrecord", label_map="./experiments/label_map.pbtxt")
+dataset = get_dataset(
+    "./data/train/*.tfrecord", label_map="./experiments/label_map.pbtxt"
+)
 
 
 def display_instances(batch):
     plt.figure()
-    img = batch['image'].numpy()
-    classes = batch['groundtruth_classes'].numpy()
-    boxes = batch['groundtruth_boxes'].numpy()
+    img = batch["image"].numpy()
+    boxes = batch["groundtruth_boxes"].numpy()
 
-    print("------------------")
-    print(len(boxes))
     # fit box to image
     img_h, img_w, channel = img.shape
-    boxes[:, (0, 2)] *= img_h
-    boxes[:, (1, 3)] *= img_w
-
-    color = {1: [1, 0, 0], 2: [0, 1, 0], 4: [0, 0, 1]}
+    boxes[:, (0, 2)] = boxes[:, (0, 2)] * img_h
+    boxes[:, (1, 3)] = boxes[:, (1, 3)] * img_w
+    color = {1: "red", 2: "blue", 4: "green"}
 
     # create a figure and axes objects(the objects that have plotting methods)
-    fig, ax = plt.subplots(1, figsize=(10, 10))
-    ax.imshow(img.astype(np.uint8))
+    fig, ax = plt.subplots(1, figsize=(4, 4))
 
     # add colored boxes
-    for box, category in zip(boxes, classes):
+    for box, classes in zip(boxes, batch["groundtruth_classes"].numpy()):
         anchor = (box[1], box[0])
         width = box[3] - box[1]
         height = box[2] - box[0]
-        rec = Rectangle(anchor, width, height, edgecolor=color[category], facecolor='none')
+        rec = Rectangle(
+            anchor,
+            width,
+            height,
+            linewidth=1,
+            edgecolor=color[classes],
+            facecolor="none",
+        )
         ax.add_patch(rec)
 
     # output
+    ax.imshow(img)
     plt.savefig("mygraph.png")
     return
 
@@ -45,4 +50,3 @@ dataset = dataset.shuffle(10)
 dataset = dataset.take(1)
 for batch in dataset:
     display_instances(batch)
-    
